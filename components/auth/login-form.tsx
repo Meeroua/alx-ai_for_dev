c1f3ee173ef25603119e3894999b7a57bc1724b8
Meroua Kemoum <merouakemoum@hotmail.com>
2025-08-29 21:39:28 +0100

    feat: Scaffold authentication project structure

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,8 @@
+export default function LoginForm() {
+  return (
+    <form>
+      <h2>Login Form</h2>
+      {/* Form fields will go here */}
+    </form>
+  );
+}
---
+++ components/auth/register-form.tsx
@@ -0,0 +1,8 @@
+export default function RegisterForm() {
+  return (
+    <form>
+      <h2>Register Form</h2>
+      {/* Form fields will go here */}
+    </form>
+  );
+}
