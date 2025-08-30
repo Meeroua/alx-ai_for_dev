b34c89e02a6b6c2451eab4f9a99897d20ba14e2e
Meroua Kemoum <merouakemoum@hotmail.com>
2025-08-30 21:42:19 +0100

    fix ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,9 @@
+'use client'
+
+import LoginForm from '@/components/auth/login-form'
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-      {/* Login form will go here */}
-    </div>
-  );
+    <LoginForm />
+  )
 }
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,8 +1,9 @@
+'use client'
+
+import RegisterForm from '@/components/auth/register-form'
+
 export default function RegisterPage() {
   return (
-    <div>
-      <h1>Register</h1>
-      {/* Register form will go here */}
-    </div>
-  );
+    <RegisterForm />
+  )
 }
